aae5f43ea425552720820783948026e464061854
Artem Reva <artem_reva@epam.com>
2023-11-07 09:26:03 +0300

    Add server-side rendering and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
+"use client";
+import * as React from 'react';
+import styles from './page.module.css';
+import Main from "@/app/components/root";
 export default function Home() {
+  const [open, setOpen] = React.useState(false);
+
   return (
     <main className={styles.main}>
-      Events & Feedbacks
+      <Main open={open} setOpen={setOpen} />
     </main>
   )
 }
